4639931ebdd610de5727fe90bf53f242212ee06c
ChoCho66 <porkdie@gmail.com>
2025-08-23 12:00:49 +0800

    update hello

--- tensorflow/lite/micro/examples/hello_world/mnist_inputs/_.sh
+++
@@ -1,2 +0,0 @@
-source /home/kycho/Documents/tflite-micro/.tflite-micro/bin/activate
-python generate_cc_arrays.py . sample0.png
--- tensorflow/lite/micro/examples/hello_world/mnist_inputs/sample_image_data.h
+++
@@ -1,4 +0,0 @@
-#include <cstdint>
-
-constexpr unsigned int g_sample_image_data_size = 784;
-extern const float g_sample_image_data[];
