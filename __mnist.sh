a60a419b1443081ca2f5aa6b9882ede2e0c708a2
ChoCho <kuanyu.cho@upbeattechtw.com>
2025-08-28 10:04:20 +0800

    update mnist

--- __mnist.sh
+++ __mnist.sh
@@ -1,4 +1,18 @@
-source /home/kycho/Documents/tflite-micro-main/.tflite-micro/bin/activate
+source /home/kycho/Documents/tflite-micro/.tflite-micro/bin/activate
+
+pushd tensorflow/lite/micro/examples/mnist/mnist_inputs > /dev/null || exit 1
+
+    # 隨機選 0~9
+    num=$(( RANDOM % 10 ))
+
+    # mnist 會去測試 sample.png 這裡隨機挑選一個
+    cp "sample${num}.png" "sample.png"
+
+    # 執行 python 腳本
+    python generate_cc_arrays.py . sample.png
+
+popd > /dev/null
+
 bazel run --jobs=12 tensorflow/lite/micro/examples/mnist:mnist_test_float
 
 # source /home/kycho/Documents/tflite-micro-main/.tflite-micro/bin/activate
--- tensorflow/lite/micro/examples/mnist/mnist_inputs/_.sh
+++ tensorflow/lite/micro/examples/mnist/mnist_inputs/_.sh
@@ -3,8 +3,8 @@ source /home/kycho/Documents/tflite-micro/.tflite-micro/bin/activate
 # 隨機選 0~9
 num=$(( RANDOM % 10 ))
 
-# 重新命名產生的檔案
-mv "sample${num}.png" "sample.png"
+# mnist 會去測試 sample.png 這裡隨機挑選一個
+cp "sample${num}.png" "sample.png"
 
 # 執行 python 腳本
 python generate_cc_arrays.py . sample.png
